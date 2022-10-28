a2a1732d8b48864ffb8fa26399cd0d036a224505
Narveen K A <narveen@techversantinfotech.com>
2022-10-28 17:53:24 +0530

    Commit

---
+++ js/accordion.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".accordion h1").click(function(){
+		var id = this.id;
+		$(".accordion-content").each(function(){
+			if($("#"+id).next()[0].id != this.id){
+				$(this).slideUp();
+			}
+		});
+	$(this).next().toggle();
+	});
+});
